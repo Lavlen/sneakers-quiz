bc19a404989eca23620fa54b2cb00c8d1b360b00
Lavlen <lavin.lennon@gmail.com>
2021-11-02 12:23:23 +0000

    create variables for holding parameters for username, answers, questions answered, new questions and options, question in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// create constants to hold the elements return from within the document
+// create variables to hold the elements return from within the document
 const homePageBox = document.querySelector(".home-page-box");
 const quizBox = document.querySelector(".questions-box");
 const scoreBox = document.querySelector(".score-box");
@@ -6,4 +6,14 @@ const questionNumber = document.querySelector(".question-number");
 const questionText = document.querySelector(".question-text");
 const optionWrapper = document.querySelector(".option-wrapper");
 const answerSection = document.querySelector(".answers-section");
+const maxNumQuestions = 10; // sets the total number of questions in the quiz
+
+let questionCounter = 0; // holds the value of the number of each question
+let questionInProgress; // holds the parameters of the current question
+let newQuestions = []; // holds new questions array
+let newOptions = []; // holds new options array
+let correctAnswers = 0; // holds the value of the number of correctly answered questions
+let answered = 0; // holds the value of the number of attempted questions
+let userName = sessionStorage.getItem("name"); // holds the value of the username entered in the form
+
 
