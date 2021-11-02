33c6f5090102f1fb9a7bad7681dcba39e765b4e2
Lavlen <lavin.lennon@gmail.com>
2021-11-02 11:54:32 +0000

    creatvariables to hol thhe element classes returned from within the document

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// create constants to hold the elements return from within the document
+const homePageBox = document.querySelector(".home-page-box");
+const quizBox = document.querySelector(".questions-box");
+const scoreBox = document.querySelector(".score-box");
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionWrapper = document.querySelector(".option-wrapper");
+const answerSection = document.querySelector(".answers-section");
+
